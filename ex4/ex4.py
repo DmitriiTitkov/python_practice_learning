ea27cae3ad3d61508cb1c061cba1c25c4db8ec3e
Dmitrii Titkov <Dmitrii_Titkov@epam.com>
2017-07-11 10:43:03 +0300

    Find simple numbers function added.

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -16,6 +16,16 @@ class DivisorFinder:
                 divisors.add(check_num)
                 divisors.add(num//check_num)
         return divisors
+    @staticmethod
+    def find_simple_numbers(start_num, end_num):
+        search_range = range(start_num, end_num)
+        simple_numbers = set()
+        for number in search_range:
+            if len(DivisorFinder.get_divisor(number)) == 2:
+                simple_numbers.add(number)
+        return sorted(simple_numbers)
+
+
 
 
 
