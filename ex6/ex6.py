d64e7701e16fc607855da27a6cf5bfe9f6711f94
Dmitrii Titkov <Dmitrii_Titkov@epam.com>
2017-07-13 15:08:50 +0300

    Spaces ignorance added. Can be passed as parameter. Default is true.

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -2,12 +2,18 @@ import math
 
 class PalindromeFinder:
     @staticmethod
-    def is_palindrome(phrase):
+    def is_palindrome(phrase, ignore_spaces=True):
+        if not isinstance(phrase, str):
+            raise Exception("Wrong data type was passed as an argument")
+        phrase = phrase.strip()
+        if ignore_spaces:
+            for character in phrase:
+                if character == " ":
+                    phrase = phrase.replace(character, "")
         phrase_length = len(phrase)
         half_to_check = math.ceil(phrase_length / 2)
         for character_index in range(0, half_to_check):
             if not phrase[character_index] == phrase[-character_index - 1]:
                 print("This is not a palindrome")
-                return False
-
-                print("This is a palindrome")
+                return
+        print("This is a palindrome")
